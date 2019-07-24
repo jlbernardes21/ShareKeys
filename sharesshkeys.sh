ba3bc11f90a246e9ae0dfc136f14f5497c7d1824
João Bernardes <joao-lb@bol.com.br>
2019-07-23 23:30:06 -0300

    Update

--- sharesshkeys.sh
+++ sharesshkeys.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 # sharekeys.sh - Shell script to share ssh keys between nodes in RAC installation
 # Author: Joao Bernardes - https://www.linkedin.com/in/jlbernardes/
-#
+# Usage: Define your password to 'orcltoshare' in all users and all nodes and run this script.
+#        After the sucessfull execution, the password can be safelly reseted. 
 
 # Basic vars
-USERS=( "oracle" "grid" "root") 
-# Get node list from standard input:
-if [ -z "$1" ]; then
-  echo "\$You need to pass the list of nodes as parameter in this script!"
-else
-  NODES="$1"
-fi
-
-# Check if the node list is
-if [[ $NODES != *","* ]]; then
-  echo -e "\nERROR: the node list needs to be passed in an incorrect format."
-  echo -e "\nExpected format: \n  $ sharesshkeys.sh node1,node2,nodeN" && exit 1
-fi
+PASSWORD="orcltoshare"
 
 # check if it is running under root 
 if [ "$(id -u)" != "0" ]; then
-  echo -e "\nError: this script needs to be executed by root. " && exit 1
+  echo -e "ERROR: this script needs to be executed by root. " && exit 1
+fi
+
+# Get node list from standard input:
+if [ "$#" -lt "2" ]; then
+  echo "ERROR: You need to pass the list of nodes as parameter in this script (at least two nodes)!"
+  echo -e "\nExpected format: \n  $ sharesshkeys.sh node1 node2 nodeN" && exit 1
+# That is broken... Fix some day...
+#else i=1 && while [ $i -ne $# ]; do
+#  currentNode="'$'$i" 
+#  echo $currentNode #NODES+=( "$""$i" ) ## Does not work, cause neasted substution is not allowed in bash...
+#  #i=$[$i+1]
+#  done
+#fi
+elif [ "$#" -eq "2" ]; then NODES=( $1 $2 )
+elif [ "$#" -eq "3" ]; then NODES=( $1 $2 $3 )
+elif [ "$#" -eq "4" ]; then NODES=( $1 $2 $3 $4 )
+elif [ "$#" -eq "5" ]; then NODES=( $1 $2 $3 $4 $5 )
+fi
+
+# Defining USERS array
+id -u grid > /dev/null 2>&1 && USERS=( "root" "oracle" "grid" )
+if [ $? -ne 0 ]; then
+  id -u oracle > /dev/null 2>&1 && USERS=( "root" "oracle" )
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Oracle user does not exists. Please create then first." && exit 1
+  fi
 fi
 
 # Identify current distro
@@ -32,12 +47,12 @@ egrep -q '^Oracle Linux Server release 6' /etc/oracle-release 2> /dev/null && DI
 egrep -q '^Oracle Linux Server release 7' /etc/oracle-release 2> /dev/null && DIST='el7'
 
 if [ -z "$DIST" ]; then
-  echo -e "\nERROR: Cannot determine the current distribuition or its not supported by this script" && exit 1
+  echo -e "ERROR: Cannot determine the current distribuition or its not supported by this script" && exit 1
 fi
 
 # Cleaning function
-cleaning(){
-  yum remove sshpass
+cleanup(){
+  yum remove -y sshpass 2>&1 > /dev/null
 }
 
 # Install dependencies (sshpass)
@@ -55,30 +70,37 @@ case $DIST in
     ;;
 esac
 
-which sshpass 2>&1 > /dev/null
-if [ $? -eq 0 ]; then
-  _SSHPASS=`which sshpass`
-else
-  yum -y install sshpass 2>&1 > /dev/null
-  if [ $? -eq 0 ]; then
-    _SSHPASS=`which sshpass`
-  else
-    rpm -Uvh $RPMURL 2>&1 > /dev/null
-    if [ $? -eq 0 ]; then
-      _SSHPASS=`which sshpass`
-    else
-      echo "\nERROR: Pre-req installation failed (sshpass)" && exit 1
+which sshpass > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Installing the RPM package SSHPASS. This is a pre-requisite for this script."
+  yum -y install sshpass > /dev/null 2>&1
+  which sshpass > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    rpm -Uvh $RPMURL > /dev/null 2>&1 
+    which sshpass > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+      echo "ERROR: Prereq installation failed (sshpass)" && exit 1
     fi
   fi
 fi
 
-# Converting string tempnodelist (basic string) to array
-bkpNODES="$NODES"
-bkpIFS="$IFS"
-IFS=',()][' read -r -a NODES <<<"([$bkpNODES])"
-IFS="$bkpIFS"
+# Defining $_SSHPASS
+_SSHPASS="sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no "
 
-echo ${NODES[@]}
+# Creating Keys
+for n in "${NODES[@]}"; do
+  for u in "${USERS[@]}"; do 
+    echo " > Executando a criação de chave para $u@$n"
+    $_SSHPASS $u@$n ssh-keygen -b 2048 -t rsa -q -N ""
+    $_SSHPASS $u@$n
+  done
+done
+
+
+
+# Sharing Keys
+
+
 
 #for i in ${NODES[@]}; do
 #  ssh $i ssh-keygen (parametros pra gerar autmoaticamente RSA e DSA) 
@@ -89,5 +111,17 @@ echo ${NODES[@]}
 #    done
 #  done  
 #done
+#echo ${NODES[@]}
+#echo ${USERS[@]}
+
+echo "Do you want to remove the SSHPASS package? [y/N]" && read REMOVE
+if [ -z "$REMOVE" ] || [ "$REMOVE" == "N" ] || [ "$REMOVE" == "n" ]; then
+  echo "Skipping sshpass de-install!"
+else if [ "$REMOVE" == "y" ]; then
+  cleanup
+else
+  echo "ERROR: Failure while de-installing SSHPASS. Please, run 'yum remove sshpass' manually." && exit 1
+fi
+fi  
 
 exit 0
